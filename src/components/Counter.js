6f01ffe74e79c4cf7889087b57aba47dce947280
Souji-hub <idebussoujan@gmail.com>
2022-06-23 17:18:05 +0545

    icons-fun

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import { FaBeer } from "react-icons/fa";
+import { GiWaterDrop } from "react-icons/gi";
 import React from "react";
 
-const Counter =() => {
-
-        const [count,setCount] = useState(0);
-        
-        return(
-            <div>
-                <button onClick={()=> setCount(count+1) } >+</button> 
-                <h1>{count}</h1>
-                <button onClick={()=> setCount(count >1? count-1:count)}>-</button>
-            </div>
-        )
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+    const [hovered, setHovered]= useState(false);
+  return (
+    <div>
+      <FaBeer
+        size={32}
+        color={count >= 4 ? "red" : "green"}
+        onClick={() => setCount(count + 1)}
+      />
+      <h1 style={{ color: `${hovered ? "yellow" : "blue"}` }}
+       onMouseOver={()=>setHovered(true)}
+       >{count}</h1>
+     <GiWaterDrop 
+     size={32}
+     color={count >= 4 ? "blue" : "green"}
+     onClick={() => setCount(count > 1 ? count - 1 : count)}/>
+    </div>
+  );
+};
 
 export default Counter;
